60ff7d2f6d31c2f1e29b18bed95cfb1c203f62a6
alex.fan <alex.fan@okchem.com>
2019-01-07 15:51:03 +0800

    charts

--- src/client.js
+++ src/client.js
@@ -1,1 +1,14 @@
-okchart.init()
+/* 
+  clientid : 
+  charts   :
+*/
+okchart.init({
+  clientid: '',
+  authContainer: 'embed-api-auth-container',
+  charts: [{
+    type: 'uv-pv', // to identify both types of chart and selector
+    container: 'chart-container',
+    selector: 'view-selector-container',
+    options: {}
+  }]
+})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,92 @@
-(function(win) {
+(function (win, doc) {
+  (function (w, d, s, g, js, fs) {
+    g = w.gapi || (w.gapi = {});
+    g.analytics = {
+      q: [],
+      ready: function (f) {
+        this.q.push(f);
+      }
+    };
+    js = d.createElement(s);
+    fs = d.getElementsByTagName(s)[0];
+    js.src = 'https://apis.google.com/js/platform.js';
+    fs.parentNode.insertBefore(js, fs);
+    js.onload = function () {
+      g.load('analytics');
+    };
+  }(win, doc, 'script'));
+
   const OKCHART = () => {
-    const _init = () => {
-      console.log("Hi, I am okchart");
+    const _init = ({
+      clientid,
+      authContainer,
+      charts
+    }) => {
+      const {
+        container,
+        selector
+      } = charts[0]
+
+      gapi.analytics.ready(function () {
+
+        /**
+         * Authorize the user immediately if the user has already granted access.
+         * If no access has been created, render an authorize button inside the
+         * element with the ID "embed-api-auth-container".
+         */
+        gapi.analytics.auth.authorize({
+          container: authContainer,
+          clientid: clientid
+        });
+
+
+        /**
+         * Create a new ViewSelector instance to be rendered inside of an
+         * element with the id "view-selector-container".
+         */
+        var viewSelector = new gapi.analytics.ViewSelector({
+          container: selector
+        });
+
+        // Render the view selector to the page.
+        viewSelector.execute();
+
+
+        /**
+         * Create a new DataChart instance with the given query parameters
+         * and Google chart options. It will be rendered inside an element
+         * with the id "chart-container".
+         */
+        var dataChart = new gapi.analytics.googleCharts.DataChart({
+          query: {
+            metrics: 'ga:pageviews',
+            dimensions: 'ga:date',
+            'start-date': '30daysAgo',
+            'end-date': 'yesterday'
+          },
+          chart: {
+            container: container,
+            type: 'LINE',
+            options: {
+              width: '100%'
+            }
+          }
+        });
+
+
+        /**
+         * Render the dataChart on the page whenever a new view is selected.
+         */
+        viewSelector.on('change', function (ids) {
+          dataChart.set({
+            query: {
+              ids: ids
+            }
+          }).execute();
+        });
+
+      });
+
     };
 
     return {
@@ -9,4 +94,4 @@
     };
   };
   win.okchart = OKCHART();
-})(window);
+})(window, document);
