5baaa97353ccf6feae5f08695fdc245d8d3cf78e
alex.fan <alex.fan@okchem.com>
2019-01-15 16:03:46 +0800

    remove unused code

--- src/Chart/c-pv-uv.js
+++ src/Chart/c-pv-uv.js
@@ -22,33 +22,6 @@ PvUvChart.prototype = {
   },
   // function 3: render
   render: function (data) {
-    // this.chart.source(data);
-    // this.chart.scale('value', {
-    //   min: 0
-    // });
-    // this.chart.scale('day', {
-    //   range: [0, 1]
-    // });
-    // this.chart.tooltip({
-    //   crosshairs: {
-    //     type: 'line'
-    //   }
-    // });
-    // this.chart.line().position('day*value').shape('smooth');
-    // this.chart.point().position('day*value').size(4).shape('circle').style({
-    //   stroke: '#fff',
-    //   lineWidth: 1
-    // });
-    // // 如何格式化坐标轴文本
-    // // https://www.yuque.com/antv/g2-docs/tutorial-faq#vs5rwy
-    // this.chart.axis('day', {
-    //   label: {
-    //     formatter: val => {
-    //       return `${val.slice(4, 6)}/${val.slice(6, 8)}`;
-    //     }
-    //   }
-    // });
-    // this.chart.render();
     var ds = new DataSet();
     var dv = ds.createView().source(data);
     // fold 方式完成了行列转换，如果不想使用 DataSet 直接手工转换数据即可
