6f764632801761b6fbe06f76755eef8779b028ec
fancn <fanyj@yikeer.com>
2019-01-21 20:01:34 +0800

    reload with default param

--- src/Presenter/p-dashboard.js
+++ src/Presenter/p-dashboard.js
@@ -5,16 +5,18 @@ import buildQueryConverter from '../Factory/buildQueryConverter'
 const Presenter = function({ views, models }) {
   this.views = views
   this.models = models
-  this.querys = {}
+  this.queryParams = {}
   this.ids = null
 }
 
 Presenter.prototype = {
   init: function() {
     const self = this
+
     // hook up to ids update
     events.attach('ids', ({ ids }) => {
       self.ids = ids
+      this.reload()
     })
 
     // hook up to model update
@@ -29,15 +31,26 @@ Presenter.prototype = {
 
     // hook up to change event of view's selector
     Object.keys(self.views).forEach(key => {
+      // default params for each type
+      self.queryParams[key] = {
+        ids: self.ids,
+      }
       self.views[key].init({
         onSelectorChange: data => {
           const queryConverter = buildQueryConverter({
             type: key,
           })
+          // new param against old param
+          const queryParam = {
+            ...self.queryParams[key], // override old param
+            ids: self.ids,
+            ...data,
+          }
+          // update old param
+          self.queryParams[key] = queryParam
           self.models[key].fetch(
             queryConverter({
-              ids: self.ids,
-              ...data,
+              ...queryParam,
             })
           )
         },
@@ -48,6 +61,22 @@ Presenter.prototype = {
     if (key && Object.keys(this.views).includes(key)) {
       this.views[key].render(data)
     }
+  },
+  reload: function() {
+    Object.keys(this.models).forEach(key => {
+      const queryConverter = buildQueryConverter({
+        type: key,
+      })
+      const queryParam = {
+        ...this.queryParams[key],
+      }
+      this.models[key].fetch(
+        queryConverter({
+          ...queryParam,
+          ids: this.ids,
+        })
+      )
+    })
   },
 }
 
