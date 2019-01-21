ebbeb979e013f89f9cd20d705d444a1ddcf25220
fancn <fanyj@yikeer.com>
2019-01-21 14:42:03 +0800

    selector default selection

--- src/Utils/HtmlElementBuilder.js
+++ src/Utils/HtmlElementBuilder.js
@@ -4,6 +4,9 @@ const createSelect = ({ options }) => {
   options.forEach(option => {
     const z = document.createElement('option')
     z.setAttribute('value', option.value)
+    if (option.selected) {
+      z.setAttribute('selected', 'selected')
+    }
     var t = document.createTextNode(option.text)
     z.appendChild(t)
     select.appendChild(z)
