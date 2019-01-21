3dad22071dd5c4677b0641628642556b67e7d3d8
fancn <fanyj@yikeer.com>
2019-01-21 19:02:29 +0800

    code refactor

--- src/Factory/buildView.js
+++ src/Factory/buildView.js
@@ -3,7 +3,9 @@ import PvUvView from '../View/v-pv-uv'
 const buildView = ({ type, containerId }) => {
   switch (type) {
     case 'pv-uv':
-      return new PvUvView(containerId)
+      return new PvUvView({
+        chartContainerId: containerId,
+      })
     default:
       return null
   }
