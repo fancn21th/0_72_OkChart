6914e10e87db6b88e02058e5c23a4f65d3b4fd14
Snow <snow.xiao@okchem.cn>
2019-01-24 14:44:38 +0800

    买家注册数折线图和买家分布饼图

--- src/Factory/buildModel.js
+++ src/Factory/buildModel.js
@@ -1,12 +1,18 @@
 import PvModel from '../Model/m-pv-uv'
+import BuyersRegistModel from '../Model/m-buyers-regist'
+import BuyersRegistDistributiontModel from '../Model/m-buyers-regist-distribution'
 
 const buildModel = ({ type, query }) => {
-  switch (type) {
-    case 'pv-uv':
-      return new PvModel(query)
-    default:
-      return null
-  }
+    switch (type) {
+        case 'pv-uv':
+            return new PvModel(query)
+        case 'buyers-regist':
+            return new BuyersRegistModel(query)
+        case 'buyers-regist-distribution':
+            return new BuyersRegistDistributiontModel(query)
+        default:
+            return null
+    }
 }
 
 export default buildModel
--- src/Factory/buildQueryConverter.js
+++ src/Factory/buildQueryConverter.js
@@ -1,12 +1,18 @@
 import convertPvUvQuery from '../Converter/Query/c-q-pv-uv'
+import convertBuyersRegistQuery from '../Converter/Query/c-q-buyers-regist'
+import convertBuyersRegistDistributionQuery from '../Converter/Query/c-q-buyers-regist-distribution'
 
 const buildQueryConverter = ({ type }) => {
-  switch (type) {
-    case 'pv-uv':
-      return convertPvUvQuery
-    default:
-      return null
-  }
+    switch (type) {
+        case 'pv-uv':
+            return convertPvUvQuery
+        case 'buyers-regist':
+            return convertBuyersRegistQuery
+        case 'buyers-regist-distribution':
+            return convertBuyersRegistDistributionQuery
+        default:
+            return null
+    }
 }
 
 export default buildQueryConverter
