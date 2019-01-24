ea4e41eb3e2ed7a88707bc96c970df36855027a5
Snow <snow.xiao@okchem.cn>
2019-01-24 17:25:44 +0800

    去掉 buildQueryConverter.js

--- src/Model/m-buyers-regist-distribution.js
+++ src/Model/m-buyers-regist-distribution.js
@@ -1,12 +1,13 @@
 import events from '../Utils/events'
 import convert from '../Converter/Data/c-d-buyers-regist-distribution'
-
+import queryConvert from '../Converter/Query/c-q-buyers-regist-distribution'
 const Model = function(query) {
     this.query = query
 }
 
 Model.prototype = {
-    fetch: function(params) {
+    fetch: function(selectorData) {
+        const params = queryConvert(selectorData)
         this.query.query(params).then(response => {
             const data = convert(response.rows)
                 // TODO: debugger console
