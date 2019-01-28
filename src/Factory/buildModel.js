d6379c60c4af1fd47c68cd94e780611c59cf08bc
Snow <snow.xiao@okchem.cn>
2019-01-28 11:15:07 +0800

    修改参数

--- src/Factory/buildModel.js
+++ src/Factory/buildModel.js
@@ -8,20 +8,20 @@ import TopBrowserModel from '../Model/m-top-browser'
 import browserGrowthModel from '../Model/m-browser-growth'
 
 const buildModel = ({ type, query }) => {
-  switch (type) {
-    case 'pv-uv':
-      return new PvModel(query)
-    case 'distribution':
-      return [new DistributionModel(query), new UserGrowthModel(query)]
-    case 'top-browser':
-      return [new TopBrowserModel(query), new browserGrowthModel(query)]
-    case 'buyers-regist':
-      return new BuyersRegistModel(query)
-    case 'buyers-regist-distribution':
-      return new BuyersRegistDistributiontModel(query)
-    default:
-      return null
-  }
+    switch (type) {
+        case 'pv-uv':
+            return new PvModel(query)
+        case 'distribution':
+            return [new DistributionModel(query), new UserGrowthModel(query)]
+        case 'top-browser':
+            return [new TopBrowserModel(query), new browserGrowthModel(query)]
+        case 'buyers-regist':
+            return new BuyersRegistModel(query)
+        case 'buyers-regist-distribution':
+            return [new BuyersRegistDistributiontModel(query), new BuyersRegistGrowthModel(query)]
+        default:
+            return null
+    }
 }
 
 export default buildModel
