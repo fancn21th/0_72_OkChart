90104be999e20405e23346fbdfb604edd7a230e6
Snow <snow.xiao@okchem.cn>
2019-01-30 20:40:20 +0800

    渠道改为 source

--- src/Converter/Query/c-q-suppliers-regist-growth.js
+++ src/Converter/Query/c-q-suppliers-regist-growth.js
@@ -1,7 +1,7 @@
 const convert = ({ ids, timespan, timeUnit, startDate, endDate, countryBrowser }) => {
     const startDateStr = startDate || `${timespan*2 || '60'}daysAgo`
     const enDateStr = `${timespan || '30'}daysAgo`
-    const dimensionsStr = countryBrowser || `ga:browser`
+    const dimensionsStr = countryBrowser || `ga:source`
     const param = {
             ids,
             metrics: 'ga:goal1Completions',
