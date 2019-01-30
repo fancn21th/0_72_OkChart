d9a7dfd0225984616616ec069b90393cfc6c6e1a
Snow <snow.xiao@okchem.cn>
2019-01-30 20:56:34 +0800

    注册卖家部分 加标题和调整selector的位置

--- src/View/Selector/sel-suppliers-regist.js
+++ src/View/Selector/sel-suppliers-regist.js
@@ -3,27 +3,30 @@ import DateRangeSelector from './Control/date-range-selector'
 import TimeUnitSelector from './Control/time-unit-selector'
 
 const Selector = function({ chartContainerId }) {
-  this.chartContainerId = chartContainerId
-  this.chartContainer = document.getElementById(this.chartContainerId)
-  this.selectorList = [
-    new TimespanSelector(),
-    new DateRangeSelector(),
-    new TimeUnitSelector(),
-  ]
+    this.chartContainerId = chartContainerId
+    this.chartContainer = document.getElementById(this.chartContainerId)
+    this.selectorList = [
+        new TimespanSelector(),
+        new DateRangeSelector(),
+        new TimeUnitSelector(),
+    ]
 }
 
 Selector.prototype = {
-  init: function({ onSelectorChange }) {
-    let query = {}
-    const onChangeHandler = data => {
-      query = Object.assign(query, data)
-      onSelectorChange(query)
-    }
-    this.selectorList.forEach(selector => {
-      selector.init({ onSelectorChange: onChangeHandler })
-      selector.appendTo(this.chartContainer)
-    })
-  },
+    init: function({ onSelectorChange }) {
+        let query = {}
+        const onChangeHandler = data => {
+            query = Object.assign(query, data)
+            onSelectorChange(query)
+        }
+        this.selectorList.forEach(selector => {
+            selector.init({ onSelectorChange: onChangeHandler })
+            const selectorContainer = this.chartContainer.getElementsByClassName(
+                'selector-container'
+            )[0]
+            selector.appendTo(selectorContainer)
+        })
+    },
 }
 
 export default Selector
