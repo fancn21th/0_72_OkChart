09a75ee6551b394790300747cde5e2e89f3bcff4
Snow <snow.xiao@okchem.cn>
2019-01-30 20:41:03 +0800

    增长部分 修改

--- src/Converter/Data/c-d-suppliers-regist-growth.js
+++ src/Converter/Data/c-d-suppliers-regist-growth.js
@@ -1,19 +1,12 @@
 const convert = (collection, currentData) => {
-    // 去掉当前数据中的others
-    currentData.pop();
-
     const growthArr = currentData.map(e => {
-        // 取排名前十的国家
-        var top10 = collection.reverse().slice(0, 10).map(item => ({
-            item: item[0],
-            value: parseInt(item[1], 10)
-        }))
-
         let changeData = null;
-        collection.forEach(item => {
-            if (item[0] === e.item) {
-                const addData = e.count - parseInt(item[1]);
+        collection.forEach(element => {
+            if (element[0] === e.item) {
+                const addData = e.count - parseInt(element[1]);
                 changeData = { item: e.item, value: addData }
+            } else {
+                changeData = { item: e.item, value: e.count }
             }
         })
         return changeData
