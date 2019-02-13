ba703d7de4efffe6676fbb28b23d3d966a1a4245
alex.fan <alex.fan@okchem.com>
2019-02-13 17:15:42 +0800

    注册买家数 refactor

--- src/View/v-buyers-regist.js
+++ src/View/v-buyers-regist.js
@@ -1,19 +1,25 @@
+import SuperView, { inheritPrototype } from './Base/SuperView'
 import BuyersRegistChart from './Chart/c-buyers-regist'
 import BuyersRegistSelector from './Selector/sel-buyers-regist'
 
 const View = function({ chartContainerId }) {
-    this.chart = new BuyersRegistChart({ chartContainerId })
-    this.selector = new BuyersRegistSelector({ chartContainerId })
+  SuperView.call(this, { chartContainerId, title: '注册买家数' })
+  this.chart = new BuyersRegistChart({ chartContainerId: this.chartWrapperId })
+  this.selector = new BuyersRegistSelector({
+    chartContainerId: this.selectorWrapperId,
+  })
 }
 
+inheritPrototype(View, SuperView)
+
 View.prototype = {
-    init: function({ onSelectorChange }) {
-        this.chart.init()
-        this.selector.init({ onSelectorChange })
-    },
-    render: function(data) {
-        this.chart.render(data)
-    },
+  init: function({ onSelectorChange }) {
+    this.chart.init()
+    this.selector.init({ onSelectorChange })
+  },
+  render: function(data) {
+    this.chart.render(data)
+  },
 }
 
 export default View
