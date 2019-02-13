07b44e0c7603364c1c88840df014ea6156381eed
alex.fan <alex.fan@okchem.com>
2019-02-13 17:21:59 +0800

    注册买家分布 refactor

--- src/View/v-buyers-regist-distribution.js
+++ src/View/v-buyers-regist-distribution.js
@@ -1,37 +1,41 @@
+import SuperView, { inheritPrototype } from './Base/SuperView'
 import BuyersRegistDistributionChart from './Chart/c-buyers-regist-distribution'
 import BuyersRegistGrowthChart from './Chart/c-buyers-regist-growth'
 import BuyersRegistDistributionSelector from './Selector/sel-buyers-regist-distribution'
 import { createDiv } from '../Utils/HtmlElementBuilder'
 
 const View = function({ chartContainerId }) {
-    this.chartContainerId = chartContainerId
-    this.selector = new BuyersRegistDistributionSelector({ chartContainerId })
-    this.chart1 = null
-    this.chart2 = null
+  SuperView.call(this, { chartContainerId, title: '注册买家分布' })
+
+  const chartId1 = 'buyers-distribution'
+  const chartId2 = 'buyers-up'
+  const chartContainer1 = createDiv(chartId1)
+  const chartContainer2 = createDiv(chartId2)
+
+  const chartWrapper = document.getElementById(this.chartWrapperId)
+  chartWrapper.appendChild(chartContainer1)
+  chartWrapper.appendChild(chartContainer2)
+
+  this.chart1 = new BuyersRegistDistributionChart({
+    chartContainerId: chartId1,
+  })
+  this.chart2 = new BuyersRegistGrowthChart({ chartContainerId: chartId2 })
+
+  this.selector = new BuyersRegistDistributionSelector({
+    chartContainerId: this.selectorWrapperId,
+  })
 }
 
 View.prototype = {
-    init: function({ onSelectorChange }) {
-        const chartId1 = "buyers-distribution";
-        const chartId2 = "buyers-up";
-        // 创建两个图表的容器
-        const chartContainer1 = createDiv(chartId1);
-        const chartContainer2 = createDiv(chartId2);
-        const chartTopContainer = document.getElementById(this.chartContainerId);
-        chartTopContainer.appendChild(chartContainer1);
-        chartTopContainer.appendChild(chartContainer2);
-
-        this.chart1 = new BuyersRegistDistributionChart({ chartContainerId: chartId1 });
-        this.chart1.init();
-        this.chart2 = new BuyersRegistGrowthChart({ chartContainerId: chartId2 });
-        this.chart2.init();
-
-        this.selector.init({ onSelectorChange })
-    },
-    render: function({ data1, data2 }) {
-        if (data1) this.chart1.render(data1)
-        if (data2) this.chart2.render(data2)
-    },
+  init: function({ onSelectorChange }) {
+    this.chart1.init()
+    this.chart2.init()
+    this.selector.init({ onSelectorChange })
+  },
+  render: function({ data1, data2 }) {
+    if (data1) this.chart1.render(data1)
+    if (data2) this.chart2.render(data2)
+  },
 }
 
 export default View
