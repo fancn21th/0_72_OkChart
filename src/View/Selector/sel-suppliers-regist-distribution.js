008ac5e6511c203c484e954f8c930f598a26ab27
alex.fan <alex.fan@okchem.com>
2019-02-13 17:27:38 +0800

    注册卖家分布 refactor

--- src/View/Selector/sel-suppliers-regist-distribution.js
+++ src/View/Selector/sel-suppliers-regist-distribution.js
@@ -4,31 +4,28 @@ import TimeUnitSelector from './Control/time-unit-selector'
 import CountryBrowserSelector from './Control/country-browser-selector'
 
 const Selector = function({ chartContainerId }) {
-    this.chartContainerId = chartContainerId
-    this.chartContainer = document.getElementById(this.chartContainerId)
-    this.selectorList = [
-        new TimespanSelector(),
-        new DateRangeSelector(),
-        // new TimeUnitSelector(),
-        new CountryBrowserSelector()
-    ]
+  this.chartContainerId = chartContainerId
+  this.chartContainer = document.getElementById(this.chartContainerId)
+  this.selectorList = [
+    new TimespanSelector(),
+    new DateRangeSelector(),
+    // new TimeUnitSelector(),
+    new CountryBrowserSelector(),
+  ]
 }
 
 Selector.prototype = {
-    init: function({ onSelectorChange }) {
-        let query = {}
-        const onChangeHandler = data => {
-            query = Object.assign(query, data)
-            onSelectorChange(query)
-        }
-        this.selectorList.forEach(selector => {
-            selector.init({ onSelectorChange: onChangeHandler })
-            const selectorContainer = this.chartContainer.getElementsByClassName(
-                'selector-container'
-            )[0]
-            selector.appendTo(selectorContainer)
-        })
-    },
+  init: function({ onSelectorChange }) {
+    let query = {}
+    const onChangeHandler = data => {
+      query = Object.assign(query, data)
+      onSelectorChange(query)
+    }
+    this.selectorList.forEach(selector => {
+      selector.init({ onSelectorChange: onChangeHandler })
+      selector.appendTo(this.chartContainer)
+    })
+  },
 }
 
 export default Selector
--- src/View/v-suppliers-regist-distribution.js
+++ src/View/v-suppliers-regist-distribution.js
@@ -1,37 +1,41 @@
+import SuperView, { inheritPrototype } from './Base/SuperView'
 import SuppliersRegistDistributionChart from './Chart/c-suppliers-regist-distribution'
 import SuppliersRegistGrowthChart from './Chart/c-suppliers-regist-growth'
 import SuppliersRegistDistributionSelector from './Selector/sel-suppliers-regist-distribution'
 import { createDiv } from '../Utils/HtmlElementBuilder'
 
 const View = function({ chartContainerId }) {
-    this.chartContainerId = chartContainerId
-    this.selector = new SuppliersRegistDistributionSelector({ chartContainerId })
-    this.chart1 = null
-    this.chart2 = null
+  SuperView.call(this, { chartContainerId, title: '注册卖家分布' })
+
+  const chartId1 = 'suppliers-distribution'
+  const chartId2 = 'suppliers-up'
+  const chartContainer1 = createDiv(chartId1)
+  const chartContainer2 = createDiv(chartId2)
+
+  const chartWrapper = document.getElementById(this.chartWrapperId)
+  chartWrapper.appendChild(chartContainer1)
+  chartWrapper.appendChild(chartContainer2)
+
+  this.chart1 = new SuppliersRegistDistributionChart({
+    chartContainerId: chartId1,
+  })
+  this.chart2 = new SuppliersRegistGrowthChart({ chartContainerId: chartId2 })
+
+  this.selector = new SuppliersRegistDistributionSelector({
+    chartContainerId: this.selectorWrapperId,
+  })
 }
 
 View.prototype = {
-    init: function({ onSelectorChange }) {
-        const chartId1 = "suppliers-distribution";
-        const chartId2 = "suppliers-up";
-        // 创建两个图表的容器
-        const chartContainer1 = createDiv(chartId1);
-        const chartContainer2 = createDiv(chartId2);
-        const chartTopContainer = document.getElementById(this.chartContainerId);
-        chartTopContainer.appendChild(chartContainer1);
-        chartTopContainer.appendChild(chartContainer2);
-
-        this.chart1 = new SuppliersRegistDistributionChart({ chartContainerId: chartId1 });
-        this.chart1.init();
-        this.chart2 = new SuppliersRegistGrowthChart({ chartContainerId: chartId2 });
-        this.chart2.init();
-
-        this.selector.init({ onSelectorChange })
-    },
-    render: function({ data1, data2 }) {
-        if (data1) this.chart1.render(data1)
-        if (data2) this.chart2.render(data2)
-    },
+  init: function({ onSelectorChange }) {
+    this.chart1.init()
+    this.chart2.init()
+    this.selector.init({ onSelectorChange })
+  },
+  render: function({ data1, data2 }) {
+    if (data1) this.chart1.render(data1)
+    if (data2) this.chart2.render(data2)
+  },
 }
 
 export default View
