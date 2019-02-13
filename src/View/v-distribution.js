d335edc54a59e322274ab465e6338281a79b90dd
alex.fan <alex.fan@okchem.com>
2019-02-13 16:55:10 +0800

    访问分布 refactor

--- src/View/v-distribution.js
+++ src/View/v-distribution.js
@@ -1,28 +1,34 @@
+import SuperView, { inheritPrototype } from './Base/SuperView'
 import DistributionChart from '../View/Chart/c-distribution'
 import UserGrowthChart from '../View/Chart/c-user-growth'
 import DistributionSelector from '../View/Selector/sel-distribution'
 import { createDiv } from '../Utils/HtmlElementBuilder'
 
 const View = function({ chartContainerId }) {
-  this.chartContainerId = chartContainerId
-  this.selector = new DistributionSelector({ chartContainerId })
-  this.chart1 = null
-  this.chart2 = null
+  SuperView.call(this, { chartContainerId, title: '访问分布' })
+
+  const chartId1 = 'distribution-container'
+  const chartId2 = 'user-growth-container'
+  const chartContainer1 = createDiv(chartId1)
+  const chartContainer2 = createDiv(chartId2)
+
+  const chartWrapper = document.getElementById(this.chartWrapperId)
+  chartWrapper.appendChild(chartContainer1)
+  chartWrapper.appendChild(chartContainer2)
+
+  this.chart1 = new DistributionChart({ chartContainerId: chartId1 })
+  this.chart2 = new UserGrowthChart({ chartContainerId: chartId2 })
+
+  this.selector = new DistributionSelector({
+    chartContainerId: this.selectorWrapperId,
+  })
 }
+
+inheritPrototype(View, SuperView)
 
 View.prototype = {
   init: function({ onSelectorChange }) {
-    const chartId1 = 'distribution-container'
-    const chartId2 = 'user-growth-container'
-    const chartContainer1 = createDiv(chartId1)
-    const chartContainer2 = createDiv(chartId2)
-    const chartTopContainer = document.getElementById(this.chartContainerId)
-    chartTopContainer.appendChild(chartContainer1)
-    chartTopContainer.appendChild(chartContainer2)
-
-    this.chart1 = new DistributionChart({ chartContainerId: chartId1 })
     this.chart1.init()
-    this.chart2 = new UserGrowthChart({ chartContainerId: chartId2 })
     this.chart2.init()
     this.selector.init({ onSelectorChange })
   },
