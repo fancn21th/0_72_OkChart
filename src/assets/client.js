ee38043295d29d4ceb09e41d202354f0d5acc989
Snow <snow.xiao@okchem.cn>
2019-02-22 11:04:31 +0800

    client.js 更新

--- src/assets/client.js
+++ src/assets/client.js
@@ -2,35 +2,34 @@ okchart.init({
     clientId: '62894052188-ndunchslu23481qdh95rdogu39q5bd0l.apps.googleusercontent.com',
     authContainer: 'embed-api-auth-container',
     viewSelector: 'view-selector-container',
-    charts: [
-        // {
-        //   type: 'overview',
-        //   container: 'overview-container',
-        // },
-        // {
-        //   type: 'pv-uv',
-        //   container: 'pv-uv-container',
-        // },
-        // {
-        //   type: 'distribution',
-        //   container: 'distribution-container',
-        // },
-        // {
-        //   type: 'source-top-15',
-        //   container: 'source-top-15-container',
-        // },
-        // {
-        //   type: 'buyers-regist',
-        //   container: 'buyers-regist-container',
-        // },
+    charts: [{
+            type: 'overview',
+            container: 'overview-container',
+        },
+        {
+            type: 'pv-uv',
+            container: 'pv-uv-container',
+        },
+        {
+            type: 'distribution',
+            container: 'distribution-container',
+        },
+        {
+            type: 'source-top-15',
+            container: 'source-top-15-container',
+        },
+        {
+            type: 'buyers-regist',
+            container: 'buyers-regist-container',
+        },
         {
             type: 'buyers-regist-distribution',
             container: 'buyers-regist-distribution-container',
         },
-        // {
-        //   type: 'suppliers-regist',
-        //   container: 'suppliers-regist-container',
-        // },
+        {
+            type: 'suppliers-regist',
+            container: 'suppliers-regist-container',
+        },
         {
             type: 'suppliers-regist-distribution',
             container: 'suppliers-regist-distribution-container',
