f0655c90521e0d0622690824b90191e2e936cefa
xavier <xavier.deng@okchem.com>
2019-02-22 11:05:01 +0800

    client.js change

--- src/assets/client.js
+++ src/assets/client.js
@@ -8,10 +8,10 @@ okchart.init({
       type: 'overview',
       container: 'overview-container',
     },
-    // {
-    //   type: 'pv-uv',
-    //   container: 'pv-uv-container',
-    // },
+    {
+      type: 'pv-uv',
+      container: 'pv-uv-container',
+    },
     {
       type: 'distribution',
       container: 'distribution-container',
@@ -20,21 +20,21 @@ okchart.init({
       type: 'source-top-15',
       container: 'source-top-15-container',
     },
-    // {
-    //   type: 'buyers-regist',
-    //   container: 'buyers-regist-container',
-    // },
+    {
+      type: 'buyers-regist',
+      container: 'buyers-regist-container',
+    },
     {
       type: 'buyers-regist-distribution',
       container: 'buyers-regist-distribution-container',
     },
-    // {
-    //   type: 'suppliers-regist',
-    //   container: 'suppliers-regist-container',
-    // },
-    // {
-    //   type: 'suppliers-regist-distribution',
-    //   container: 'suppliers-regist-distribution-container',
-    // },
+    {
+      type: 'suppliers-regist',
+      container: 'suppliers-regist-container',
+    },
+    {
+      type: 'suppliers-regist-distribution',
+      container: 'suppliers-regist-distribution-container',
+    },
   ],
 })
