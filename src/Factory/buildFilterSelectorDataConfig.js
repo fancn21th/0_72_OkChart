87a9b964b266536db07a8c694e8cb44bbaf39922
alex.fan <alex.fan@okchem.com>
2019-03-05 17:12:19 +0800

    selector filter bug fix

--- src/Factory/buildFilterSelectorDataConfig.js
+++ src/Factory/buildFilterSelectorDataConfig.js
@@ -1,11 +1,18 @@
-import defaultSelectorFilterConfig from '../Config/SelectorFilter/cfg-sf-default'
+import overview from '../Config/SelectorFilter/cfg-sf-overview'
+import buyerRegistDistribution from '../Config/SelectorFilter/cfg-sf-buyer-regist-distribution'
+
+const defaultConfig = {
+  filter: null,
+}
 
 const buildSelectorFilter = ({ type }) => {
   switch (type) {
-    case 'overview':
     case 'source-top-15':
+      return defaultConfig
+    case 'overview':
+      return overview
     case 'buyers-regist-distribution':
-      return defaultSelectorFilterConfig
+      return buyerRegistDistribution
     default:
       throw new Error('OKCHART::ERROR:: no selector filter config is defined.')
   }
--- src/Utils/pipeline/filterSelectorData.js
+++ src/Utils/pipeline/filterSelectorData.js
@@ -5,18 +5,22 @@ const filter = ({
   const { filter } = buildFilterSelectorDataConfig({
     type: viewType,
   })
-  return {
-    filteredSelectorData: Object.keys(selectorData).reduce((acc, key) => {
-      if (!filter.includes(key)) {
-        return {
-          ...acc,
-          [key]: selectorData[key],
-        }
+  return filter === null
+    ? {
+        filteredSelectorData: selectorData,
       }
-      // exclude key/value pair given in filter
-      return acc
-    }, {}),
-  }
+    : {
+        filteredSelectorData: Object.keys(selectorData).reduce((acc, key) => {
+          if (!filter.includes(key)) {
+            return {
+              ...acc,
+              [key]: selectorData[key],
+            }
+          }
+          // exclude key/value pair given in filter
+          return acc
+        }, {}),
+      }
 }
 
 export default filter
