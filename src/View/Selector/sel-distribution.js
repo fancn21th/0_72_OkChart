df03cbcce7e6650960f417918e8f65e57248edc3
alex.fan <alex.fan@okchem.com>
2019-03-22 17:05:29 +0800

    update all selector

--- src/View/Selector/sel-distribution.js
+++ src/View/Selector/sel-distribution.js
@@ -1,3 +1,4 @@
+import SuperSelector, { inheritPrototype } from './Base/SuperSelector'
 import TimespanSelector from './Control/timespan-selector'
 import DateRangeSelector from './Control/date-range-selector'
 import PvuvSelector from './Control/pvuv-selector'
@@ -5,48 +6,30 @@ import CountryBrowserSelector from './Control/country-browser-selector'
 import DynamicMultipleSelector from './Control/dynamic-multiple-selector'
 import WorkingDateSelector from './Control/working-date-selector'
 
-const Selector = function ({
-  chartContainerId
-}) {
-  this.chartContainerId = chartContainerId
-  this.chartContainer = document.getElementById(this.chartContainerId)
+const Selector = function({ chartContainerId }) {
+  SuperSelector.call(this, { chartContainerId })
+
   this.sourceCountrySelector = new DynamicMultipleSelector({
     selectorType: 'sourceCountry',
     placeholder: '选择或者输入渠道/国家过滤项',
   })
-  this.selectorList = [
+
+  this.selectorList = this.selectorList.concat([
     new TimespanSelector(),
     new DateRangeSelector(),
     new PvuvSelector(),
     new CountryBrowserSelector(),
     this.sourceCountrySelector,
     new WorkingDateSelector(),
-  ]
+  ])
 }
 
-Selector.prototype = {
-  init: function ({
-    onSelectorChange
-  }) {
-    let query = {}
-    const onChangeHandler = data => {
-      query = Object.assign(query, data)
-      onSelectorChange(query)
-    }
-    this.selectorList.forEach(selector => {
-      selector.init({
-        onSelectorChange: onChangeHandler
-      })
-      selector.appendTo(this.chartContainer)
-    })
-  },
-  render: function ({
-    sourceCountryFilterCollection
-  }) {
-    this.sourceCountrySelector.render({
-      options: sourceCountryFilterCollection,
-    })
-  },
+inheritPrototype(Selector, SuperSelector)
+
+Selector.prototype.render = function({ sourceCountryFilterCollection }) {
+  this.sourceCountrySelector.render({
+    options: sourceCountryFilterCollection,
+  })
 }
 
 export default Selector
