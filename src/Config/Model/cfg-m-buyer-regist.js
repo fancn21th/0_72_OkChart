44a4c782e6959ccbed3e7eb46d9d325d54051d35
alex.fan <alex.fan@okchem.com>
2019-03-23 17:46:37 +0800

    fix bug

--- src/Config/Model/cfg-m-buyer-regist.js
+++ src/Config/Model/cfg-m-buyer-regist.js
@@ -1,21 +1,11 @@
-const convert = ({
-  responseDataSolo: {
-    responseData: collection
-  }
-}) => {
+const convert = ({ responseDataSolo: { responseData: collection } }) => {
   return {
     data: collection.map(item => ({
       day: item[0],
       buyers: parseInt(item[1], 10),
-    }))
+    })),
   }
 }
 export default {
   customConverters: [convert],
-  groupFieldIndex: 0,
-  sumFieldIndex: [1],
-  sortField: [{
-    index: 0,
-    order: 'asc',
-  }, ],
 }
