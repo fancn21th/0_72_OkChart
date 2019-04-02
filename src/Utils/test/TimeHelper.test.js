5fc4195ed836d78962b7eaa0c9b062a61aab6f60
fancn <fanyj@yikeer.com>
2019-04-02 20:30:15 +0800

    more test cases

--- src/Utils/test/TimeHelper.test.js
+++ src/Utils/test/TimeHelper.test.js
@@ -4,7 +4,7 @@ import {
 } from '../TimeHelper'
 
 describe('测试 getWorkingDateCountByMonth', () => {
-  test('201902 应该有 20 个 工作日 和 8个 非工作日', () => {
+  test('201902 应该有 20 个 工作日 和 8 个 非工作日', () => {
     // 断言
     expect(getWorkingDateCountByMonth('201902', null, null)).toEqual({
       workingDateCount: 20,
@@ -12,7 +12,7 @@ describe('测试 getWorkingDateCountByMonth', () => {
     })
   })
 
-  test('201902 应该有 19 个 工作日 和 8个 非工作日', () => {
+  test('201902 20190202 20190227 应该有 18 个 工作日 和 8 个 非工作日', () => {
     expect(
       getWorkingDateCountByMonth('201902', '20190202', '20190227')
     ).toEqual({
@@ -22,4 +22,21 @@ describe('测试 getWorkingDateCountByMonth', () => {
   })
 })
 
-describe('测试 getWorkingDateCountByWeek', () => {})
+describe('测试 getWorkingDateCountByWeek', () => {
+  test('201913 应该有 5 个 工作日 和 2 个 非工作日', () => {
+    // 断言
+    expect(getWorkingDateCountByWeek('201913', null, null)).toEqual({
+      workingDateCount: 5,
+      nonWorkingDateCount: 2,
+    })
+  })
+
+  test('201913 20190202 20190227 应该有 0 个 工作日 和 0 个 非工作日', () => {
+    expect(getWorkingDateCountByWeek('201913', '20190202', '20190227')).toEqual(
+      {
+        workingDateCount: 0,
+        nonWorkingDateCount: 0,
+      }
+    )
+  })
+})
