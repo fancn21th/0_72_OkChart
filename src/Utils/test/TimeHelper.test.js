c0adddba3f78482494e15d697372710c45b5d327
alex.fan <alex.fan@okchem.com>
2019-04-02 18:28:25 +0800

    first commit

--- src/Utils/test/TimeHelper.test.js
+++ src/Utils/test/TimeHelper.test.js
@@ -1,8 +1,25 @@
-import { getWorkingDateCountByMonth } from '../TimeHelper'
+import {
+  getWorkingDateCountByMonth,
+  getWorkingDateCountByWeek,
+} from '../TimeHelper'
 
-test('201902', () => {
-  expect(getWorkingDateCountByMonth('201902', null, null)).toEqual({
-    workingDateCount: 20,
-    nonWorkingDateCount: 8,
+describe('测试 getWorkingDateCountByMonth', () => {
+  test('201902 应该有 20 个 工作日 和 8个 非工作日', () => {
+    // 断言
+    expect(getWorkingDateCountByMonth('201902', null, null)).toEqual({
+      workingDateCount: 20,
+      nonWorkingDateCount: 8,
+    })
+  })
+
+  test('201902 应该有 19 个 工作日 和 8个 非工作日', () => {
+    expect(
+      getWorkingDateCountByMonth('201902', '20190202', '20190227')
+    ).toEqual({
+      workingDateCount: 18,
+      nonWorkingDateCount: 8,
+    })
   })
 })
+
+describe('测试 getWorkingDateCountByWeek', () => {})
