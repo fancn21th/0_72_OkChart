c0fdb72ce4584be1c2b297581f99d8b21e0b137d
alex.fan <alex.fan@okchem.com>
2019-04-23 21:28:03 +0800

    acc

--- src/Config/Model/cfg-m-ok-customer.js
+++ src/Config/Model/cfg-m-ok-customer.js
@@ -1,6 +1,19 @@
-const convert = ({ responseDataSolo: { responseData } }) => {
+import { withRowsAccumulationValue } from '../../Utils/dataTableCal'
+
+const convert = ({
+  responseDataSolo: {
+    responseData,
+    selectorData: { accumulative },
+  },
+}) => {
+  const data = accumulative ? withRowsAccumulationValue({
+    tableData: responseData,
+    ignoreColIndexes: [0],
+    ignoreRowLastIndexes: [],
+  }) : responseData
+
   return {
-    data: responseData.map(item => ({
+    data: data.map(item => ({
       day: item[0],
       count: parseInt(item[1], 10),
     })),
